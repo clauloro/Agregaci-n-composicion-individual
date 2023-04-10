a1e835e3df0349d669ac30de14f08c5e9645e3d0
clauloro <114728388+clauloro@users.noreply.github.com>
2023-04-10 11:59:25 +0200

    mas cambios

--- El_dia_siguiente/empresa.py
+++ El_dia_siguiente/empresa.py
@@ -1,54 +1,60 @@
-class Persona:
-    def __init__(self, nombre):
-        self.nombre = nombre
-
-class Empleado(Persona):
-    def __init__(self, nombre, empresa):
-        super().__init__(nombre)
-        self.empresa = empresa
-
 class Empresa:
     def __init__(self, nombre):
         self.nombre = nombre
         self.edificios = []
-
+        self.empleados = []
+        
     def agregar_edificio(self, edificio):
         self.edificios.append(edificio)
+        
+    def agregar_empleado(self, empleado):
+        self.empleados.append(empleado)
+
+class Empleado:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.ubicacion = None
 
 class Ciudad:
     def __init__(self, nombre):
         self.nombre = nombre
         self.edificios = []
-
+        
     def agregar_edificio(self, edificio):
         self.edificios.append(edificio)
-        edificio.ciudad = self
-
-    def destruir(self):
-        for edificio in self.edificios:
-            for empleado in edificio.empleados:
-                del empleado
-            del edificio
-        del self
 
 class Edificio:
-    def __init__(self, nombre):
+    def __init__(self, nombre, ciudad):
         self.nombre = nombre
-        self.empleados = []
-        self.ciudad = None
+        self.ciudad = ciudad
+        ciudad.agregar_edificio(self)
 
-    def agregar_empleado(self, empleado):
-        self.empleados.append(empleado)
-        empleado.empresa.agregar_edificio(self)
+new_york = Ciudad("New York")
+los_angeles = Ciudad("Los Ángeles")
 
-yoo_hoo = Empresa('YooHoo!')
-martin = Empleado('Sr. Martin', yoo_hoo)
-salim = Empleado('Salim', yoo_hoo)
-xing = Empleado('Sra. Xing', yoo_hoo)
-edificio_a = Edificio('A')
-edificio_b = Edificio('B')
-edificio_c = Edificio('C')
-new_york = Ciudad('New York')
-los_angeles = Ciudad('Los Angeles')
+edificio_a = Edificio("A", new_york)
+edificio_b = Edificio("B", new_york)
+edificio_c = Edificio("C", los_angeles)
 
-yoo_hoo.agregar_ed
+yoohoo = Empresa("YooHoo!")
+
+yoohoo.agregar_edificio(edificio_a)
+yoohoo.agregar_edificio(edificio_b)
+yoohoo.agregar_edificio(edificio_c)
+
+martin = Empleado("Martin")
+salim = Empleado("Salim")
+xing = Empleado("Xing")
+
+yoohoo.agregar_empleado(martin)
+yoohoo.agregar_empleado(salim)
+yoohoo.agregar_empleado(xing)
+
+def destruir_ciudad(ciudad):
+    ciudad.edificios = []
+
+destruir_ciudad(new_york)
+
+for empleado in yoohoo.empleados:
+    if empleado.ubicacion in new_york.edificios:
+        empleado.ubicacion = None
